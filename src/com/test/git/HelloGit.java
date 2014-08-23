6dcf114ff5542950b81e09526857c564abd81ee6
crusader45 <roff2@naver.com>
2014-08-23 15:14:11 +0900

    delete...

--- src/com/test/git/HelloGit.java
+++ src/com/test/git/HelloGit.java
@@ -4,9 +4,5 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("hello Git!!");
-		
-		System.out.println("second..");
-		
-		System.out.println("hello world");
 	}
 }
